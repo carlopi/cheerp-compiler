ba8d993fd7429715cc1b2a7055e502845184d84e
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-11-21 10:52:49 +0000

    Disable inlining between asmjs and genericjs, even if the function is marked always_inline

--- llvm/lib/Analysis/InlineCost.cpp
+++ llvm/lib/Analysis/InlineCost.cpp
@@ -2137,6 +2137,14 @@ InlineCost llvm::getInlineCost(
                                           " address space");
     }
 
+  //CHEERP: Do not inline normal/asmjs methods called from the other side
+  bool callerAsmJS = caller->getSection() == StringRef("asmjs");
+  bool calleeAsmJS = Callee->getSection() == StringRef("asmjs");
+  if (calleeAsmJS!= callerAsmJS)
+  {
+    return llvm::InlineCost::getNever();
+  }
+
   // Calls to functions with always-inline attributes should be inlined
   // whenever possible.
   if (Call.hasFnAttr(Attribute::AlwaysInline)) {
@@ -2172,14 +2180,6 @@ InlineCost llvm::getInlineCost(
   // Don't inline call sites marked noinline.
   if (Call.isNoInline())
     return llvm::InlineCost::getNever("noinline call site attribute");
-
-  //CHEERP: Do not inline normal/asmjs methods called from the other side
-  bool callerAsmJS = caller->getSection() == StringRef("asmjs");
-  bool calleeAsmJS = Callee->getSection() == StringRef("asmjs");
-  if (calleeAsmJS!= callerAsmJS)
-  {
-    return llvm::InlineCost::getNever();
-  }
 
   LLVM_DEBUG(llvm::dbgs() << "      Analyzing call of " << Callee->getName()
                      << "... (caller:" << Caller->getName() << ")\n");
