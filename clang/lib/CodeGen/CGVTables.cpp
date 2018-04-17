bbb655ac4899d637c3d40545042452fd5888de83
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-04-17 16:25:45 +0200

    Add the asmjs flag to anonymous structs when appropriate

--- clang/lib/CodeGen/CGVTables.cpp
+++ clang/lib/CodeGen/CGVTables.cpp
@@ -1299,9 +1299,8 @@ llvm::Type* CodeGenTypes::GetVTableSubObjectType(CodeGenModule& CGM,
   for (uint32_t i = 0; i < extraOffsets; i++) {
     VTableTypes.push_back(OffsetTy);
   }
-  llvm::StructType* ret = llvm::StructType::get(CGM.getLLVMContext(), VTableTypes, false, cast<llvm::StructType>(CGM.getTypes().GetVTableBaseType(asmjs)));
-  if (asmjs)
-    ret->setAsmJS();
+  llvm::StructType* ret = llvm::StructType::get(CGM.getLLVMContext(), VTableTypes,
+                            false, cast<llvm::StructType>(CGM.getTypes().GetVTableBaseType(asmjs)), asmjs);
   return ret;
 }
 
@@ -1344,7 +1343,7 @@ llvm::Type* CodeGenTypes::GetBasicVTableType(uint32_t virtualMethodsCount, bool 
   for(uint32_t j=0;j<virtualMethodsCount;j++)
     VTableTypes.push_back(FuncPtrTy);
 
-  return llvm::StructType::get(getLLVMContext(), VTableTypes, false, cast<llvm::StructType>(GetVTableBaseType(asmjs)));
+  return llvm::StructType::get(getLLVMContext(), VTableTypes, false, cast<llvm::StructType>(GetVTableBaseType(asmjs)), asmjs);
 }
 
 llvm::Type* CodeGenTypes::GetClassTypeInfoType()
