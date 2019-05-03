614b9100a1a66fb2ee7bd8bcd5a5abb5211f02e2
Alessandro Pignotti <a.pignotti@sssup.it>
2019-05-03 12:04:22 +0200

    Handle cheerp in a few switches

--- llvm/lib/Support/Triple.cpp
+++ llvm/lib/Support/Triple.cpp
@@ -710,6 +710,7 @@ static Triple::ObjectFormatType getDefaultFormat(const Triple &T) {
   case Triple::thumbeb:
   case Triple::ve:
   case Triple::xcore:
+  case Triple::cheerp:
     return Triple::ELF;
 
   case Triple::ppc64:
@@ -1478,6 +1479,7 @@ Triple Triple::getBigEndianArchVariant() const {
   case Triple::x86_64:
   case Triple::xcore:
   case Triple::ve:
+  case Triple::cheerp:
 
   // ARM is intentionally unsupported here, changing the architecture would
   // drop any arch suffixes.
@@ -1570,6 +1572,7 @@ bool Triple::isLittleEndian() const {
   case Triple::x86:
   case Triple::x86_64:
   case Triple::xcore:
+  case Triple::cheerp:
     return true;
   default:
     return false;
