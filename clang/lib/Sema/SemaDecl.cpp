56f863e72cafa0e8f1b938d86908e8f87d69a8f6
Carlo Piovesan <carlo.piovesan@gmail.com>
2019-10-25 21:09:57 +0200

    JsExport: Introduce checks on functions/methods declaration
    This allows to catch errors on template that may never be instantiated

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -40,6 +40,7 @@
 #include "clang/Sema/ParsedTemplate.h"
 #include "clang/Sema/Scope.h"
 #include "clang/Sema/ScopeInfo.h"
+#include "clang/Sema/SemaCheerp.h"
 #include "clang/Sema/SemaInternal.h"
 #include "clang/Sema/Template.h"
 #include "clang/Sema/TemplateDeduction.h"
@@ -8849,6 +8850,8 @@ Sema::ActOnFunctionDeclarator(Scope *S, Declarator &D, DeclContext *DC,
               ArrayRef<TemplateParameterList *>(TemplateParamLists)
                   .drop_back(1));
         }
+
+
       } else {
         // This is a function template specialization.
         isFunctionTemplateSpecialization = true;
@@ -9224,6 +9227,9 @@ Sema::ActOnFunctionDeclarator(Scope *S, Declarator &D, DeclContext *DC,
 
   // Handle attributes.
   ProcessDeclAttributes(S, NewFD, D);
+
+  //Perform all checks related to functions and methods. Currently limited to [[cheerp::jsexport]] attribute but expansible
+  cheerp::checkFunction(NewFD, *this);
 
   if (getLangOpts().OpenCL) {
     // OpenCL v1.1 s6.5: Using an address space qualifier in a function return
