71147a963f69b66433d91675452a786b8256e7ee
Carlo Piovesan <carlo.piovesan@gmail.com>
2019-10-26 20:07:07 +0200

    JsExport: Introduce class to keep [[cheerp::jsexport]] state and add checks on free functions

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -48,6 +48,7 @@
 #include "clang/Sema/IdentifierResolver.h"
 #include "clang/Sema/ObjCMethodList.h"
 #include "clang/Sema/Ownership.h"
+#include "clang/Sema/SemaCheerp.h"
 #include "clang/Sema/Scope.h"
 #include "clang/Sema/SemaConcept.h"
 #include "clang/Sema/TypoCorrection.h"
@@ -11924,6 +11925,7 @@ protected:
   friend class ASTWriter;
 
 public:
+  cheerp::CheerpSemaData cheerpSemaData;
   /// Retrieve the keyword associated
   IdentifierInfo *getNullabilityKeyword(NullabilityKind nullability);
 
