8366be769c52c1fd0ea0e73f0ea325948d3df2c8
Alessandro Pignotti <a.pignotti@sssup.it>
2020-12-18 17:08:41 +0000

    Adapt to builder API changes

--- clang/lib/CodeGen/CGDecl.cpp
+++ clang/lib/CodeGen/CGDecl.cpp
@@ -1817,7 +1817,7 @@ void CodeGenFunction::EmitAutoVarInit(const AutoVarEmission &emission) {
       Init && emission.IsEscapingByRef && isCapturedBy(D, Init);
 
   bool locIsByrefHeader = !capturedByInit;
-  const Address Loc =
+  Address Loc =
       locIsByrefHeader ? emission.getObjectAddress(*this) : emission.Addr;
 
   // Note: constexpr already initializes everything correctly.
@@ -1882,7 +1882,7 @@ void CodeGenFunction::EmitAutoVarInit(const AutoVarEmission &emission) {
 
   if (D.getType()->isArrayType())
   {
-    Loc = Builder.CreateConstArrayGEP(Loc, 0, CharUnits());
+    Loc = Builder.CreateConstArrayGEP(Loc, 0);
   }
 
   llvm::Type *BP = CGM.Int8Ty->getPointerTo(Loc.getAddressSpace());
