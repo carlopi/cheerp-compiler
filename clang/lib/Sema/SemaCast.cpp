leaningtech/cheerp-clang

Mark casts for malloc and free as duetto safe

lib/Sema/SemaCast.cpp
@@ -21,6 +21,7 @@
 #include "clang/AST/ExprObjC.h"
 #include "clang/AST/RecordLayout.h"
 #include "clang/Basic/PartialDiagnostic.h"
+#include "clang/Basic/TargetBuiltins.h"
 #include "clang/Sema/Initialization.h"
 #include "llvm/ADT/SmallVector.h"
 #include <set>
@@ -91,6 +92,19 @@ namespace {
                                             Self.Context.ARCUnbridgedCastTy,
                                             CK_Dependent, castExpr, 0,
                                             castExpr->getValueKind());
+      }
+      //Duetto: Mark cast as safe for some builtins
+      if (CallExpr* CE = dyn_cast<CallExpr>(castExpr->getSubExpr()))
+      {
+        if (FunctionDecl* FDecl = CE->getDirectCallee())
+        {
+          if (unsigned builtin = FDecl->getBuiltinID())
+          {
+            // Casting a value returned from malloc it's ok
+            if (builtin == Builtin::BImalloc)
+              castExpr->setDuettoSafe(true);
+          }
+        }
       }
       return Self.Owned(castExpr);
     }

carlopi/cheerp-compiler

clang/lib/Sema/SemaCast.cpp
@@ -21,6 +21,7 @@
 #include "clang/AST/RecordLayout.h"
 #include "clang/Basic/PartialDiagnostic.h"
 #include "clang/Basic/TargetInfo.h"
+#include "clang/Basic/TargetBuiltins.h"
 #include "clang/Lex/Preprocessor.h"
 #include "clang/Sema/Initialization.h"
 #include "llvm/ADT/SmallVector.h"
@@ -109,6 +110,19 @@ namespace {
                                             castExpr->getValueKind());
       }
       updatePartOfExplicitCastFlags(castExpr);
+      //Cheerp: Mark cast as safe for some builtins
+      if (CallExpr* CE = dyn_cast<CallExpr>(castExpr->getSubExpr()))
+      {
+        if (FunctionDecl* FDecl = CE->getDirectCallee())
+        {
+          if (unsigned builtin = FDecl->getBuiltinID())
+          {
+            // Casting a value returned from malloc it's ok
+            if (builtin == Builtin::BImalloc)
+              castExpr->setCheerpSafe(true);
+          }
+        }
+      }
       return castExpr;
     }
 