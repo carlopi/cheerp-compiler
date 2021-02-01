leaningtech/cheerp-clang

Fix construction of multi-dimensional arrays of C++ objects

lib/CodeGen/CGDecl.cpp
@@ -1449,8 +1449,6 @@ void CodeGenFunction::emitArrayDestroy(llvm::Value *begin,
                                        Destroyer *destroyer,
                                        bool checkZeroLength,
                                        bool useEHCleanup) {
-  assert(!type->isArrayType());
-
   // The basic structure here is a do-while loop, because we don't
   // need to check for the zero-element case.
   llvm::BasicBlock *bodyBB = createBasicBlock("arraydestroy.body");
@@ -1474,14 +1472,18 @@ void CodeGenFunction::emitArrayDestroy(llvm::Value *begin,
   llvm::Value *element = Builder.CreateInBoundsGEP(elementPast, negativeOne,
                                                    "arraydestroy.element");
 
-  if (useEHCleanup)
-    pushRegularPartialArrayCleanup(begin, element, type, destroyer);
+  if(type->isArrayType()) {
+    emitDestroy(element, type, destroyer, useEHCleanup);
+  } else {
+    if (useEHCleanup)
+      pushRegularPartialArrayCleanup(begin, element, type, destroyer);
 
-  // Perform the actual destruction there.
-  destroyer(*this, element, type);
+    // Perform the actual destruction there.
+    destroyer(*this, element, type);
 
-  if (useEHCleanup)
-    PopCleanupBlock();
+    if (useEHCleanup)
+      PopCleanupBlock();
+  }
 
   // Check whether we've reached the end.
   llvm::Value *done = Builder.CreateICmpEQ(element, begin, "arraydestroy.done");

carlopi/cheerp-compiler

clang/lib/CodeGen/CGDecl.cpp
@@ -2159,8 +2159,6 @@ void CodeGenFunction::emitArrayDestroy(llvm::Value *begin,
                                        Destroyer *destroyer,
                                        bool checkZeroLength,
                                        bool useEHCleanup) {
-  assert(!elementType->isArrayType());
-
   // The basic structure here is a do-while loop, because we don't
   // need to check for the zero-element case.
   llvm::BasicBlock *bodyBB = createBasicBlock("arraydestroy.body");
@@ -2184,15 +2182,19 @@ void CodeGenFunction::emitArrayDestroy(llvm::Value *begin,
   llvm::Value *element = Builder.CreateInBoundsGEP(elementPast, negativeOne,
                                                    "arraydestroy.element");
 
-  if (useEHCleanup)
-    pushRegularPartialArrayCleanup(begin, element, elementType, elementAlign,
+  if(elementType->isArrayType()) {
+    emitDestroy(Address(element, elementAlign), elementType, destroyer, useEHCleanup);
+  } else {
+    if (useEHCleanup)
+      pushRegularPartialArrayCleanup(begin, element, elementType, elementAlign,
                                    destroyer);
 
-  // Perform the actual destruction there.
-  destroyer(*this, Address(element, elementAlign), elementType);
+    // Perform the actual destruction there.
+    destroyer(*this, Address(element, elementAlign), elementType);
 
-  if (useEHCleanup)
-    PopCleanupBlock();
+    if (useEHCleanup)
+      PopCleanupBlock();
+  }
 
   // Check whether we've reached the end.
   llvm::Value *done = Builder.CreateICmpEQ(element, begin, "arraydestroy.done");