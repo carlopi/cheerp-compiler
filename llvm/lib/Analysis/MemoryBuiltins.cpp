leaningtech/cheerp-llvm

Rebranding to Cheerp

lib/Analysis/MemoryBuiltins.cpp
@@ -53,7 +53,7 @@ struct AllocFnsTy {
 // know which functions are nounwind, noalias, nocapture parameters, etc.
 static const AllocFnsTy AllocationFnData[] = {
   // Keep this one as the first one
-  {LibFunc::duetto_allocate,     CallocLike,  1, 0,  -1},
+  {LibFunc::cheerp_allocate,     CallocLike,  1, 0,  -1},
   {LibFunc::malloc,              MallocLike,  1, 0,  -1},
   {LibFunc::valloc,              MallocLike,  1, 0,  -1},
   {LibFunc::Znwj,                OpNewLike,   1, 0,  -1}, // new(unsigned int)
@@ -95,10 +95,10 @@ static Function *getCalledFunction(const Value *V, bool LookThroughBitCast) {
 static const AllocFnsTy *getAllocationData(const Value *V, AllocType AllocTy,
                                            const TargetLibraryInfo *TLI,
                                            bool LookThroughBitCast = false) {
-  // Skip all intrinsics but duetto.allocate
+  // Skip all intrinsics but cheerp.allocate
   if (const IntrinsicInst* II = dyn_cast<IntrinsicInst>(V))
   {
-    if (II->getIntrinsicID() == Intrinsic::duetto_allocate)
+    if (II->getIntrinsicID() == Intrinsic::cheerp_allocate)
       return &AllocationFnData[0];
     return nullptr;
   }

lib/Target/CheerpBackend/CheerpBackend.cpp
@@ -1,0 +1,98 @@
+//===-- CheerpBackend.cpp - Backend wrapper for CheerpWriter---------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright 2011-2014 Leaning Technologies
+//
+//===----------------------------------------------------------------------===//
+
+#include "CheerpTargetMachine.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/PassManager.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/GlobalValue.h"
+#include "llvm/IR/Type.h"
+#include "llvm/Cheerp/Writer.h"
+#include "llvm/Cheerp/AllocaMerging.h"
+#include "llvm/Cheerp/ResolveAliases.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/ToolOutputFile.h"
+
+using namespace llvm;
+
+static cl::opt<std::string> SourceMap("cheerp-sourcemap", cl::Optional,
+  cl::desc("If specified, the file name of the source map"), cl::value_desc("filename"));
+
+
+extern "C" void LLVMInitializeCheerpBackendTarget() {
+  // Register the target.
+  RegisterTargetMachine<CheerpTargetMachine> X(TheCheerpBackendTarget);
+}
+
+namespace {
+  class CheerpWritePass : public ModulePass {
+  private:
+    formatted_raw_ostream &Out;
+    static char ID;
+    void getAnalysisUsage(AnalysisUsage& AU) const;
+  public:
+    explicit CheerpWritePass(formatted_raw_ostream &o) :
+      ModulePass(ID), Out(o) { }
+    bool runOnModule(Module &M);
+  };
+} // end anonymous namespace.
+
+bool CheerpWritePass::runOnModule(Module& M)
+{
+  AliasAnalysis &AA = getAnalysis<AliasAnalysis>();
+  if (!SourceMap.empty())
+  {
+    std::error_code ErrorString;
+    tool_output_file sourceMap(SourceMap.c_str(), ErrorString, sys::fs::F_None);
+    if (ErrorString)
+    {
+       // An error occurred opening the source map file, bail out
+       llvm::report_fatal_error(ErrorString.message(), false);
+       return false;
+    }
+    cheerp::CheerpWriter writer(M, Out, AA, SourceMap, &sourceMap.os());
+    sourceMap.keep();
+    writer.makeJS();
+  }
+  else
+  {
+    cheerp::CheerpWriter writer(M, Out, AA, SourceMap, NULL);
+    writer.makeJS();
+  }
+
+  return false;
+}
+
+void CheerpWritePass::getAnalysisUsage(AnalysisUsage& AU) const
+{
+  AU.addRequired<AliasAnalysis>();
+}
+
+char CheerpWritePass::ID = 0;
+
+//===----------------------------------------------------------------------===//
+//                       External Interface declaration
+//===----------------------------------------------------------------------===//
+
+bool CheerpTargetMachine::addPassesToEmitFile(PassManagerBase &PM,
+                                           formatted_raw_ostream &o,
+                                           CodeGenFileType FileType,
+                                           bool DisableVerify,
+                                           AnalysisID StartAfter,
+                                           AnalysisID StopAfter) {
+  if (FileType != TargetMachine::CGFT_AssemblyFile) return true;
+  PM.add(createResolveAliasesPass());
+  PM.add(createAllocaMergingPass());
+  PM.add(new CheerpWritePass(o));
+  return false;
+}

carlopi/cheerp-compiler

llvm/lib/Analysis/MemoryBuiltins.cpp
@@ -71,7 +71,7 @@ struct AllocFnsTy {
 // know which functions are nounwind, noalias, nocapture parameters, etc.
 static const std::pair<LibFunc, AllocFnsTy> AllocationFnData[] = {
   // Keep this one as the first one
-  {LibFunc_duetto_allocate,     {CallocLike,  1, 0,  -1}},
+  {LibFunc_cheerp_allocate,     {CallocLike,  1, 0,  -1}},
   {LibFunc_malloc,              {MallocLike,  1, 0,  -1}},
   {LibFunc_valloc,              {MallocLike,  1, 0,  -1}},
   {LibFunc_Znwj,                {OpNewLike,   1, 0,  -1}}, // new(unsigned int)
@@ -137,7 +137,7 @@ static Optional<AllocFnsTy>
 getAllocationDataForFunction(const Function *Callee, AllocType AllocTy,
                              const TargetLibraryInfo *TLI) {
 
-  if (Callee->getIntrinsicID() == Intrinsic::duetto_allocate)
+  if (Callee->getIntrinsicID() == Intrinsic::cheerp_allocate)
     return &AllocationFnData[0];
 
   // Make sure that the function is available.

llvm/lib/Target/CheerpBackend/CheerpBackend.cpp
@@ -1,0 +1,100 @@
+//===-- CheerpBackend.cpp - Backend wrapper for CheerpWriter---------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright 2011-2014 Leaning Technologies
+//
+//===----------------------------------------------------------------------===//
+
+#include "CheerpTargetMachine.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/PassManager.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/GlobalValue.h"
+#include "llvm/IR/Type.h"
+#include "llvm/Cheerp/Writer.h"
+#include "llvm/Cheerp/AllocaMerging.h"
+#include "llvm/Cheerp/ResolveAliases.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/ToolOutputFile.h"
+
+using namespace llvm;
+
+static cl::opt<std::string> SourceMap("cheerp-sourcemap", cl::Optional,
+  cl::desc("If specified, the file name of the source map"), cl::value_desc("filename"));
+
+
+extern "C" void LLVMInitializeCheerpBackendTarget() {
+  // Register the target.
+  RegisterTargetMachine<CheerpTargetMachine> X(TheCheerpBackendTarget);
+}
+
+namespace {
+  class CheerpWritePass : public ModulePass {
+  private:
+    raw_ostream &Out;
+    static char ID;
+    void getAnalysisUsage(AnalysisUsage& AU) const;
+  public:
+    explicit CheerpWritePass(raw_ostream &o) :
+      ModulePass(ID), Out(o) { }
+    bool runOnModule(Module &M);
+  };
+} // end anonymous namespace.
+
+bool CheerpWritePass::runOnModule(Module& M)
+{
+  AliasAnalysis &AA = getAnalysis<AliasAnalysis>();
+  if (!SourceMap.empty())
+  {
+    std::error_code ErrorString;
+    tool_output_file sourceMap(SourceMap.c_str(), ErrorString, sys::fs::F_None);
+    if (ErrorString)
+    {
+       // An error occurred opening the source map file, bail out
+       llvm::report_fatal_error(ErrorString.message(), false);
+       return false;
+    }
+    cheerp::CheerpWriter writer(M, Out, AA, SourceMap, &sourceMap.os());
+    sourceMap.keep();
+    writer.makeJS();
+  }
+  else
+  {
+    cheerp::CheerpWriter writer(M, Out, AA, SourceMap, NULL);
+    writer.makeJS();
+  }
+
+  return false;
+}
+
+void CheerpWritePass::getAnalysisUsage(AnalysisUsage& AU) const
+{
+  AU.addRequired<AliasAnalysis>();
+}
+
+char CheerpWritePass::ID = 0;
+
+//===----------------------------------------------------------------------===//
+//                       External Interface declaration
+//===----------------------------------------------------------------------===//
+
+bool CheerpTargetMachine::addPassesToEmitFile(PassManagerBase &PM,
+                                           raw_pwrite_stream &o,
+                                           CodeGenFileType FileType,
+                                           bool DisableVerify,
+                                           AnalysisID StartBefore,
+                                           AnalysisID StartAfter,
+                                           AnalysisID StopAfter,
+                                           MachineFunctionInitializer* MFInit) {
+  if (FileType != TargetMachine::CGFT_AssemblyFile) return true;
+  PM.add(createResolveAliasesPass());
+  PM.add(createAllocaMergingPass());
+  PM.add(new CheerpWritePass(o));
+  return false;
+}