leaningtech/cheerp-clang

Fix upcasting of const pointers

lib/CodeGen/CGClass.cpp
@@ -338,10 +338,14 @@ CodeGenFunction::GenerateUpcast(llvm::Value* Value,
   if(Value->getType()!=BasePtrTy)
   {
     //This should become an intrinsic
-    llvm::Instruction* castI=cast<llvm::Instruction>(Builder.CreateBitCast(Value, BasePtrTy));
-    llvm::MDNode* md=llvm::MDNode::get(getLLVMContext(), llvm::SmallVector<llvm::Value*, 1>());
-    castI->setMetadata("duetto.upcast.collapsed", md);
-    Value=castI;
+    Value=Builder.CreateBitCast(Value, BasePtrTy);
+    //TODO: How to handle constexpr cases?
+    if(llvm::Instruction::classof(Value))
+    {
+      llvm::Instruction* castI=cast<llvm::Instruction>(Value);
+      llvm::MDNode* md=llvm::MDNode::get(getLLVMContext(), llvm::SmallVector<llvm::Value*, 1>());
+      castI->setMetadata("duetto.upcast.collapsed", md);
+    }
   }
   return Value;
 }

carlopi/cheerp-compiler

clang/lib/CodeGen/CGClass.cpp
@@ -504,10 +504,14 @@ CodeGenFunction::GenerateUpcast(llvm::Value* Value,
   if(Value->getType()!=BasePtrTy)
   {
     //This should become an intrinsic
-    llvm::Instruction* castI=cast<llvm::Instruction>(Builder.CreateBitCast(Value, BasePtrTy));
-    llvm::MDNode* md=llvm::MDNode::get(getLLVMContext(), llvm::SmallVector<llvm::Metadata*, 1>());
-    castI->setMetadata("duetto.upcast.collapsed", md);
-    Value=castI;
+    Value=Builder.CreateBitCast(Value, BasePtrTy);
+    //TODO: How to handle constexpr cases?
+    if(llvm::Instruction::classof(Value))
+    {
+      llvm::Instruction* castI=cast<llvm::Instruction>(Value);
+      llvm::MDNode* md=llvm::MDNode::get(getLLVMContext(), llvm::SmallVector<llvm::Metadata*, 1>());
+      castI->setMetadata("duetto.upcast.collapsed", md);
+    }
   }
   return Value;
 }