leaningtech/cheerp-clang

Disable system wide headers when the target is duetto

lib/Basic/Targets.cpp
@@ -5204,7 +5204,7 @@ static TargetInfo *AllocateTarget(const llvm::Triple &Triple) {
     return NULL;
 
   case llvm::Triple::duetto:
-    return new LinuxTargetInfo<DuettoTargetInfo>(Triple);
+    return new DuettoTargetInfo(Triple);
 
   case llvm::Triple::hexagon:
     return new HexagonTargetInfo(Triple);

carlopi/cheerp-compiler

clang/lib/Basic/Targets.cpp
@@ -114,7 +114,7 @@ TargetInfo *AllocateTarget(const llvm::Triple &Triple,
     return new ARCTargetInfo(Triple, Opts);
 
   case llvm::Triple::cheerp:
-    return new LinuxTargetInfo<CheerpTargetInfo>(Triple);
+    return new CheerpTargetInfo(Triple);
 
   case llvm::Triple::xcore:
     return new XCoreTargetInfo(Triple, Opts);