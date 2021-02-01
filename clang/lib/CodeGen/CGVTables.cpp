leaningtech/cheerp-clang

Rework handling of thunks

lib/CodeGen/CGVTables.cpp
@@ -34,7 +34,7 @@ CodeGenVTables::CodeGenVTables(CodeGenModule &CGM)
 
 llvm::Constant *CodeGenModule::GetAddrOfThunk(GlobalDecl GD, 
                                               const ThunkInfo &Thunk) {
-  const CXXMethodDecl* OriginalMethod = Thunk.This.Method;
+  const CXXMethodDecl* OriginalMethod = Thunk.Method;
   const CXXMethodDecl *MD = cast<CXXMethodDecl>(GD.getDecl());
 
   // Compute the mangled name.
@@ -390,7 +390,7 @@ void CodeGenFunction::GenerateThunk(llvm::Function *Fn,
                                     const CGFunctionInfo &FnInfo,
                                     GlobalDecl GD, const ThunkInfo &Thunk) {
   const CXXMethodDecl* OriginalMethod = getTarget().isByteAddressable() ? cast<CXXMethodDecl>(GD.getDecl()) :
-                                                                          Thunk.This.Method;
+                                                                          Thunk.Method;
   StartThunk(Fn, GD, FnInfo, OriginalMethod);
 
   // Get our callee.
@@ -411,7 +411,7 @@ void CodeGenFunction::GenerateThunk(llvm::Function *Fn,
 
 void CodeGenVTables::emitThunk(GlobalDecl GD, ThunkInfo Thunk,
                                bool ForVTable) {
-  const CXXMethodDecl* OriginalMethod = Thunk.This.Method;
+  const CXXMethodDecl* OriginalMethod = Thunk.Method;
   bool byteAddressable = CGM.getTarget().isByteAddressable();
   const CGFunctionInfo &FnInfo = CGM.getTypes().arrangeGlobalDeclaration(
                 byteAddressable?GD:GD.getWithDecl(OriginalMethod));

carlopi/cheerp-compiler

clang/lib/CodeGen/CGVTables.cpp
@@ -466,7 +466,7 @@ void CodeGenFunction::generateThunk(llvm::Function *Fn,
                                     const ThunkInfo &Thunk,
                                     bool IsUnprototyped) {
   const CXXMethodDecl* OriginalMethod = getTarget().isByteAddressable() ? cast<CXXMethodDecl>(GD.getDecl()) :
-                                                                          Thunk.This.Method;
+                                                                          Thunk.Method;
   StartThunk(Fn, GD, FnInfo, IsUnprototyped, OriginalMethod);
   // Create a scope with an artificial location for the body of this function.
   auto AL = ApplyDebugLocation::CreateArtificial(*this);
@@ -512,7 +512,7 @@ llvm::Constant *CodeGenVTables::maybeEmitThunk(GlobalDecl GD,
                                                ThunkInfo TI,
                                                bool ForVTable) {
   bool byteAddressable = CGM.getTarget().isByteAddressable();
-  const CXXMethodDecl* OriginalMethod = Thunk.This.Method;
+  const CXXMethodDecl* OriginalMethod = TI.Method;
   const CXXMethodDecl *MD = cast<CXXMethodDecl>(GD.getDecl());
 
   // Override the non virtual offset in bytes with the topological offset on NBA targets
@@ -546,7 +546,6 @@ llvm::Constant *CodeGenVTables::maybeEmitThunk(GlobalDecl GD,
 
   // Arrange a function prototype appropriate for a function definition. In some
   // cases in the MS ABI, we may need to build an unprototyped musttail thunk.
-  const CXXMethodDecl* OriginalMethod = TI.This.Method;
   const CGFunctionInfo &FnInfo =
       IsUnprototyped ? CGM.getTypes().arrangeUnprototypedMustTailThunk(MD)
                      : CGM.getTypes().arrangeGlobalDeclaration(