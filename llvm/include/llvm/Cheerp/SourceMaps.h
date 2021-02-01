leaningtech/cheerp-llvm

CheerpBackend: Add support for -cheerp-sourcemap-prefix

include/llvm/Cheerp/SourceMaps.h
@@ -25,6 +25,7 @@ class SourceMapGenerator
 private:
 	llvm::tool_output_file sourceMap;
 	const std::string& sourceMapName;
+	const std::string& sourceMapPrefix;
 	llvm::LLVMContext& Ctx;
 	std::map<llvm::MDString*, uint32_t> fileMap;
 	uint32_t lastFile;
@@ -34,8 +35,8 @@ private:
 	bool lineStart;
 	void writeBase64VLQInt(int32_t i);
 public:
-	// sourceMapName life span should be longer than the one of the SourceMapGenerator
-	SourceMapGenerator(const std::string& sourceMapName, llvm::LLVMContext& C, std::string& ErrorString);
+	// sourceMapName and sourceMapPrefix life spans should be longer than the one of the SourceMapGenerator
+	SourceMapGenerator(const std::string& sourceMapName, const std::string& sourceMapPrefix, llvm::LLVMContext& C, std::string& ErrorString);
 	void setDebugLoc(const llvm::DebugLoc& debugLoc);
 	void clearDebugInfo()
 	{

lib/CheerpWriter/SourceMaps.cpp
@@ -17,9 +17,9 @@ using namespace llvm;
 namespace cheerp
 {
 
-SourceMapGenerator::SourceMapGenerator(const std::string& sourceMapName, llvm::LLVMContext& C, std::string& ErrorString):
-	sourceMap(sourceMapName.c_str(), ErrorString, sys::fs::F_None), sourceMapName(sourceMapName), Ctx(C), lastFile(0),
-	lastLine(0), lastColoumn(0), validInfo(false), lineStart(true)
+SourceMapGenerator::SourceMapGenerator(const std::string& sourceMapName, const std::string& sourceMapPrefix, llvm::LLVMContext& C, std::string& ErrorString):
+	sourceMap(sourceMapName.c_str(), ErrorString, sys::fs::F_None), sourceMapName(sourceMapName), sourceMapPrefix(sourceMapPrefix),
+	Ctx(C), lastFile(0), lastLine(0), lastColoumn(0), validInfo(false), lineStart(true)
 {
 }
 
@@ -114,8 +114,11 @@ void SourceMapGenerator::endFile()
 		// Fix slashes in the file path
 		std::string tmp;
 		StringRef string=files[i]->getString();
+		unsigned start=0;
+		if(string.startswith(sourceMapPrefix))
+			start=sourceMapPrefix.size();
 		tmp.reserve(string.size());
-		for(unsigned i=0;i<string.size();i++)
+		for(unsigned i=start;i<string.size();i++)
 		{
 			char c=string[i];
 			if(c=='\\')

lib/Target/CheerpBackend/CheerpBackend.cpp
@@ -29,6 +29,9 @@ using namespace llvm;
 static cl::opt<std::string> SourceMap("cheerp-sourcemap", cl::Optional,
   cl::desc("If specified, the file name of the source map"), cl::value_desc("filename"));
 
+static cl::opt<std::string> SourceMapPrefix("cheerp-sourcemap-prefix", cl::Optional,
+  cl::desc("If specified, this prefix will be removed from source map file paths"), cl::value_desc("path"));
+
 static cl::opt<bool> PrettyCode("cheerp-pretty-code", cl::desc("Generate human-readable JS") );
 
 static cl::opt<bool> NoRegisterize("cheerp-no-registerize", cl::desc("Disable registerize pass") );
@@ -59,7 +62,7 @@ bool CheerpWritePass::runOnModule(Module& M)
   if (!SourceMap.empty())
   {
     std::string ErrorString;
-    sourceMapGenerator = new cheerp::SourceMapGenerator(SourceMap, M.getContext(), ErrorString);
+    sourceMapGenerator = new cheerp::SourceMapGenerator(SourceMap, SourceMapPrefix, M.getContext(), ErrorString);
     if (!ErrorString.empty())
     {
        // An error occurred opening the source map file, bail out

carlopi/cheerp-compiler

llvm/include/llvm/Cheerp/SourceMaps.h
@@ -25,6 +25,7 @@ class SourceMapGenerator
 private:
 	llvm::tool_output_file sourceMap;
 	const std::string& sourceMapName;
+	const std::string& sourceMapPrefix;
 	llvm::LLVMContext& Ctx;
 	std::map<llvm::MDString*, uint32_t> fileMap;
 	uint32_t lastFile;
@@ -34,8 +35,8 @@ private:
 	bool lineStart;
 	void writeBase64VLQInt(int32_t i);
 public:
-	// sourceMapName life span should be longer than the one of the SourceMapGenerator
-	SourceMapGenerator(const std::string& sourceMapName, llvm::LLVMContext& C, std::error_code& ErrorCode);
+	// sourceMapName and sourceMapPrefix life spans should be longer than the one of the SourceMapGenerator
+	SourceMapGenerator(const std::string& sourceMapName, const std::string& sourceMapPrefix, llvm::LLVMContext& C, std::error_code& ErrorCode);
 	void setDebugLoc(const llvm::DebugLoc& debugLoc);
 	void clearDebugInfo()
 	{

llvm/lib/CheerpWriter/SourceMaps.cpp
@@ -18,9 +18,9 @@ using namespace llvm;
 namespace cheerp
 {
 
-SourceMapGenerator::SourceMapGenerator(const std::string& sourceMapName, llvm::LLVMContext& C, std::error_code& ErrorCode):
-	sourceMap(sourceMapName.c_str(), ErrorCode, sys::fs::F_None), sourceMapName(sourceMapName), Ctx(C), lastFile(0),
-	lastLine(0), lastColoumn(0), validInfo(false), lineStart(true)
+SourceMapGenerator::SourceMapGenerator(const std::string& sourceMapName, const std::string& sourceMapPrefix, llvm::LLVMContext& C, std::error_code& ErrorCode):
+	sourceMap(sourceMapName.c_str(), ErrorCode, sys::fs::F_None), sourceMapName(sourceMapName), sourceMapPrefix(sourceMapPrefix),
+	Ctx(C), lastFile(0), lastLine(0), lastColoumn(0), validInfo(false), lineStart(true)
 {
 }
 
@@ -115,8 +115,11 @@ void SourceMapGenerator::endFile()
 		// Fix slashes in the file path
 		std::string tmp;
 		StringRef string=files[i]->getString();
+		unsigned start=0;
+		if(string.startswith(sourceMapPrefix))
+			start=sourceMapPrefix.size();
 		tmp.reserve(string.size());
-		for(unsigned i=0;i<string.size();i++)
+		for(unsigned i=start;i<string.size();i++)
 		{
 			char c=string[i];
 			if(c=='\\')

llvm/lib/Target/CheerpBackend/CheerpBackend.cpp
@@ -29,6 +29,9 @@ using namespace llvm;
 static cl::opt<std::string> SourceMap("cheerp-sourcemap", cl::Optional,
   cl::desc("If specified, the file name of the source map"), cl::value_desc("filename"));
 
+static cl::opt<std::string> SourceMapPrefix("cheerp-sourcemap-prefix", cl::Optional,
+  cl::desc("If specified, this prefix will be removed from source map file paths"), cl::value_desc("path"));
+
 static cl::opt<bool> PrettyCode("cheerp-pretty-code", cl::desc("Generate human-readable JS") );
 
 static cl::opt<bool> NoRegisterize("cheerp-no-registerize", cl::desc("Disable registerize pass") );
@@ -59,7 +62,7 @@ bool CheerpWritePass::runOnModule(Module& M)
   if (!SourceMap.empty())
   {
     std::error_code ErrorCode;
-    sourceMapGenerator = new cheerp::SourceMapGenerator(SourceMap, M.getContext(), ErrorCode);
+    sourceMapGenerator = new cheerp::SourceMapGenerator(SourceMap, SourceMapPrefix, M.getContext(), ErrorCode);
     if (ErrorCode)
     {
        // An error occurred opening the source map file, bail out