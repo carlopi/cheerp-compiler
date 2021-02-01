leaningtech/cheerp-clang

Support pointers to virtual member functions

lib/CodeGen/CGVTables.cpp
@@ -403,7 +403,11 @@ void CodeGenFunction::GenerateThunk(llvm::Function *Fn,
   // Get our callee.
   llvm::Type *Ty =
     CGM.getTypes().GetFunctionType(CGM.getTypes().arrangeGlobalDeclaration(GD));
-  llvm::Value *Callee = CGM.GetAddrOfFunction(GD, Ty, /*ForVTable=*/true);
+  llvm::Value *Callee = nullptr;
+  if(Thunk.isMemberPointerThunk && OriginalMethod->isVirtual())
+    Callee = CGM.getCXXABI().getVirtualFunctionPointer(*this, OriginalMethod, LoadCXXThis(), Ty);
+  else
+    Callee = CGM.GetAddrOfFunction(GD, Ty, /*ForVTable=*/true);
 
   // Make the call and return the result.
   EmitCallAndReturnForThunk(Callee, &Thunk);

carlopi/cheerp-compiler

clang/lib/CodeGen/CGVTables.cpp
@@ -486,7 +486,11 @@ void CodeGenFunction::generateThunk(llvm::Function *Fn,
   else
     Ty = CGM.getTypes().GetFunctionType(FnInfo);
 
-  llvm::Constant *Callee = CGM.GetAddrOfFunction(GD, Ty, /*ForVTable=*/true);
+  llvm::Constant *Callee = nullptr;
+  if(Thunk.isMemberPointerThunk && OriginalMethod->isVirtual())
+    Callee = CGM.getCXXABI().getVirtualFunctionPointer(*this, OriginalMethod, LoadCXXThis(), Ty, SourceLocation());
+  else
+    Callee = CGM.GetAddrOfFunction(GD, Ty, /*ForVTable=*/true);
 
   // Fix up the function type for an unprototyped musttail call.
   if (IsUnprototyped)