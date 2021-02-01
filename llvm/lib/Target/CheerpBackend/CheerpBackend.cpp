leaningtech/cheerp-llvm

Implement the CFGStackifier algorithm, Relooper's successor

lib/Target/CheerpBackend/CheerpBackend.cpp
@@ -92,7 +92,7 @@ bool CheerpWritePass::runOnModule(Module& M)
   }
 
   cheerp::NameGenerator namegen(M, GDA, registerize, PA, ReservedNames, PrettyCode);
-  cheerp::CheerpWriter writer(M, Out, PA, registerize, GDA, linearHelper, namegen, allocaStoresExtractor, memOut.get(), AsmJSMemFile,
+  cheerp::CheerpWriter writer(M, *this, Out, PA, registerize, GDA, linearHelper, namegen, allocaStoresExtractor, memOut.get(), AsmJSMemFile,
           sourceMapGenerator.get(), PrettyCode, MakeModule, NoRegisterize, !NoNativeJavaScriptMath,
           !NoJavaScriptMathImul, !NoJavaScriptMathFround, !NoCredits, MeasureTimeToMain, CheerpHeapSize,
           BoundsCheck, SymbolicGlobalsAsmJS, std::string(), ForceTypedArrays);
@@ -117,6 +117,8 @@ void CheerpWritePass::getAnalysisUsage(AnalysisUsage& AU) const
   AU.addRequired<cheerp::PointerAnalyzer>();
   AU.addRequired<cheerp::Registerize>();
   AU.addRequired<cheerp::AllocaStoresExtractor>();
+  AU.addRequired<DominatorTreeWrapperPass>();
+  AU.addRequired<LoopInfo>();
 }
 
 char CheerpWritePass::ID = 0;

lib/Target/CheerpWasmBackend/CheerpWasmBackend.cpp
@@ -116,7 +116,7 @@ bool CheerpWasmWritePass::runOnModule(Module& M)
                                     PrettyCode, cheerpMode);
     wasmWriter.makeWasm();
 
-    cheerp::CheerpWriter writer(M, jsOut, PA, registerize, GDA, linearHelper, namegen, allocaStoresExtractor, nullptr, std::string(),
+    cheerp::CheerpWriter writer(M, *this, jsOut, PA, registerize, GDA, linearHelper, namegen, allocaStoresExtractor, nullptr, std::string(),
             sourceMapGenerator, PrettyCode, MakeModule, NoRegisterize, !NoNativeJavaScriptMath,
             !NoJavaScriptMathImul, !NoJavaScriptMathFround, !NoCredits, MeasureTimeToMain, CheerpHeapSize,
             BoundsCheck, SymbolicGlobalsAsmJS, WasmFile, ForceTypedArrays);
@@ -140,6 +140,8 @@ void CheerpWasmWritePass::getAnalysisUsage(AnalysisUsage& AU) const
   AU.addRequired<cheerp::PointerAnalyzer>();
   AU.addRequired<cheerp::Registerize>();
   AU.addRequired<cheerp::AllocaStoresExtractor>();
+  AU.addRequired<DominatorTreeWrapperPass>();
+  AU.addRequired<LoopInfo>();
 }
 
 char CheerpWasmWritePass::ID = 0;

carlopi/cheerp-compiler

llvm/lib/Target/CheerpBackend/CheerpBackend.cpp
@@ -92,7 +92,7 @@ bool CheerpWritePass::runOnModule(Module& M)
   }
 
   cheerp::NameGenerator namegen(M, GDA, registerize, PA, ReservedNames, PrettyCode);
-  cheerp::CheerpWriter writer(M, Out, PA, registerize, GDA, linearHelper, namegen, allocaStoresExtractor, memOut.get(), AsmJSMemFile,
+  cheerp::CheerpWriter writer(M, *this, Out, PA, registerize, GDA, linearHelper, namegen, allocaStoresExtractor, memOut.get(), AsmJSMemFile,
           sourceMapGenerator.get(), PrettyCode, MakeModule, NoRegisterize, !NoNativeJavaScriptMath,
           !NoJavaScriptMathImul, !NoJavaScriptMathFround, !NoCredits, MeasureTimeToMain, CheerpHeapSize,
           BoundsCheck, SymbolicGlobalsAsmJS, std::string(), ForceTypedArrays);
@@ -117,6 +117,8 @@ void CheerpWritePass::getAnalysisUsage(AnalysisUsage& AU) const
   AU.addRequired<cheerp::PointerAnalyzer>();
   AU.addRequired<cheerp::Registerize>();
   AU.addRequired<cheerp::AllocaStoresExtractor>();
+  AU.addRequired<DominatorTreeWrapperPass>();
+  AU.addRequired<LoopInfoWrapperPass>();
 }
 
 char CheerpWritePass::ID = 0;

llvm/lib/Target/CheerpWasmBackend/CheerpWasmBackend.cpp
@@ -116,7 +116,7 @@ bool CheerpWasmWritePass::runOnModule(Module& M)
                                     PrettyCode, cheerpMode);
     wasmWriter.makeWasm();
 
-    cheerp::CheerpWriter writer(M, jsOut, PA, registerize, GDA, linearHelper, namegen, allocaStoresExtractor, nullptr, std::string(),
+    cheerp::CheerpWriter writer(M, *this, jsOut, PA, registerize, GDA, linearHelper, namegen, allocaStoresExtractor, nullptr, std::string(),
             sourceMapGenerator, PrettyCode, MakeModule, NoRegisterize, !NoNativeJavaScriptMath,
             !NoJavaScriptMathImul, !NoJavaScriptMathFround, !NoCredits, MeasureTimeToMain, CheerpHeapSize,
             BoundsCheck, SymbolicGlobalsAsmJS, WasmFile, ForceTypedArrays);
@@ -140,6 +140,8 @@ void CheerpWasmWritePass::getAnalysisUsage(AnalysisUsage& AU) const
   AU.addRequired<cheerp::PointerAnalyzer>();
   AU.addRequired<cheerp::Registerize>();
   AU.addRequired<cheerp::AllocaStoresExtractor>();
+  AU.addRequired<DominatorTreeWrapperPass>();
+  AU.addRequired<LoopInfoWrapperPass>();
 }
 
 char CheerpWasmWritePass::ID = 0;